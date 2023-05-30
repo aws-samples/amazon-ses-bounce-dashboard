b3d2f881b35b2d614351ec72b74c5c3d753e6c12
Claudio Miranda <cronosunder@gmail.com>
2023-05-30 13:35:12 -0400

    fix: SQS Policy

--- src/ses_queue_handler/SesNotification.js
+++ src/ses_queue_handler/SesNotification.js
@@ -13,26 +13,18 @@ const ddbClient = new DynamoDBClient({region: process.env.AWS_REGION});
 
 
 export const handler = async (event, context) => {
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    //console.info(event);
     const record = event.Records[0]
 
     const sqsBody = JSON.parse(record.body)
-    // console.log(body)
 
     const timestamp = sqsBody.Timestamp
-    const id = sqsBody.MessageId
-
     const message = JSON.parse(sqsBody.Message)
     // All event definition. For more information, see
     // https://docs.aws.amazon.com/ses/latest/dg/event-publishing-retrieving-sns-examples.html
     const type = message.eventType
-    const destination = message.mail.destination
-    const source = message.mail.source
+    const mail = message.mail
     const messageId = message.mail.messageId
-    const detail = message[type.toLowerCase()] || {}
-    // console.log("Params")
+    const event_detail = message[type.toLowerCase()] || {}
 
     const putParams = {
         TableName: process.env.TABLE_NAME,
@@ -40,17 +32,17 @@ export const handler = async (event, context) => {
             id: messageId,
             timestamp: timestamp,
             type: type,
-            from: source,
             recipient: destination,
-            detail: detail
+            event:event_detail,
+            mail
         }
     };
-    // console.log(putParams)
     try {
         const data = await ddbClient.send(new PutCommand(putParams));
         // console.log(data);
     } catch (err) {
         console.log("Error in writing data to the DynamoDB table : ", err.message)
+        throw err
     }
 }
 
