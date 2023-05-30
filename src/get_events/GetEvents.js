a6a3fd4efa4575114767147665bb70e92a5a4c5e
Claudio Miranda <cronosunder@gmail.com>
2023-05-30 15:53:00 -0400

    feat: Filter SES event by id

--- src/get_events/GetEvents.js
+++ src/get_events/GetEvents.js
@@ -4,48 +4,81 @@
 'use strict'
 
 
-
 // Create service client module using ES6 syntax.
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
-import {  unmarshall } from "@aws-sdk/util-dynamodb";
+import {DynamoDBClient, ScanCommand} from "@aws-sdk/client-dynamodb";
+import {unmarshall} from "@aws-sdk/util-dynamodb";
 
 // Create the DynamoDB Document client.
-const ddbClient = new DynamoDBClient({ region: process.env.AWS_REGION  });
+const ddbClient = new DynamoDBClient({region: process.env.AWS_REGION});
 
+const posiblesValues = {
+    "id": "S",
+    "mail": {
+        "destination": "S"
+    }
+}
+const prepareRequest = (parameter, configuration = posiblesValues) => {
+    let filter = {
+        "ExpressionAttributeNames": {},
+        "ExpressionAttributeValues": {},
+        "FilterExpression": [],
+    }
+    const condicion = "AND"
+    let index = 0
+    for (const key in parameter) {
+        if (key.indexOf(".") >= 0) {
+            console.log('key', key)
+            const partes = key.split(".")
+            const subKey = partes[0]
+            const newKey = partes.slice(1).join(".")
+            //TODO: Ver como buscar sub objetos
+        } else if (configuration[key]) {
+            const type = configuration[key]
+            filter["ExpressionAttributeNames"]["#n" + index] = key
+            filter["ExpressionAttributeValues"][":v" + index] = {
+                [type]: parameter[key].join()
+            }
+            filter["FilterExpression"].push(`#n${index} = :v${index}`)
 
+            index++
+        }
+    }
+
+    return {
+        ...filter,
+        "FilterExpression": filter["FilterExpression"].join(` ${condicion} `)
+    }
+}
 export const handler = async (event, context) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`This API only accept GET method, you tried: ${event.httpMethod}`);
     }
-    
-    console.info('received:', event);
+
     let data = {}
     let items = {}
-    
+
     let params = {
-        TableName : process.env.TABLE_NAME,
-        ExclusiveStartKey: null
+        TableName: process.env.TABLE_NAME,
+        ExclusiveStartKey: null,
+        ...prepareRequest(event.multiValueQueryStringParameters)
     };
 
-
     try {
+        // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/classes/scancommand.html
         data = await ddbClient.send(new ScanCommand(params));    // Retrieve the data from the DynamoDB table
-        items = data.Items.map( (item) => {                      // The data will be in the format  { "S" : "Bounce"}, so unmarshall it to remove
+        items = data.Items.map((item) => {                      // The data will be in the format  { "S" : "Bounce"}, so unmarshall it to remove
             return unmarshall(item);                             // the "S"
         });
-
     } catch (err) {
         console.log("Error in retrieving data from the DynamoDB table : ", err.message)
     }
-
     // Return the items to client with status code: 200
-    const response =  {
+    const response = {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*", // Allow from anywhere 
-            "Access-Control-Allow-Methods": "GET" // Allow only GET request 
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            "Access-Control-Allow-Methods": "GET" // Allow only GET request
         },
         body: JSON.stringify(items)
     };
